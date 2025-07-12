b7438b44bf485b148f0ff0f963179a2ceaa20db2
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-07-12 18:46:42 +0300

    initial

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,18 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T):Record<string | number | symbol, T[]> {
+  const groupedResult: Record<string | number | symbol, T[]> = {}:
+
+  for (const item of items) {
+    const groupValue = item[key];
+
+    if (!groupedResult[groupValue as string | number | symbol]) {
+      groupedResult[groupValue as string | number | symbol] = [];
+    }
+
+    groupedResult[groupValue as string | number | symbol].push(item);
+  }
+
+  return groupedResult;
 }
